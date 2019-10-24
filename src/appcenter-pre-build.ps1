a3aef8edc83e8e83113276e63de227245c29b06b
Xonshiz <kanojia24.10@gmail.com>
2019-10-24 03:54:45 -0400

    Initializing Code
    Added Support Files.

---
+++ src/appcenter-pre-build.ps1
@@ -0,0 +1,11 @@
+# Predefined Variables
+ $adSdkUrl = "https://admediator.gallerycdn.vsassets.io/extensions/admediator/microsoftadvertisingsdk/10.1.00/1548989510766/MicrosoftAdvertisingSDK.msi"
+ $adSdkPath = Join-Path $env:TEMP "MicrosoftAdvertisingSDK.msi"
+ 
+# Download the files to local temp folder
+ Write-Output "downloading $adSdkUrl…"
+ Invoke-WebRequest -Uri $adSdkUrl -OutFile $adSDKPath
+ 
+# Install the SDKs (use the "qn" flag to install silently)
+ Write-Output "installing $adSdkPath…"
+ Start-Process $adSdkPath -ArgumentList "/q" -Wait
